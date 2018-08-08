92b3c962b23b09c4705ece0ebedc899c7f717b03
Tim Fischer <emailtimfischer@gmail.com>
2018-08-08 12:45:39 -0600

    Import input to app

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import './compontent-css/Input.css';
 
 export default class Input extends Component {
   constructor() {
     super();
+    this.state = {
+      titleInput: '',
+      bodyInput: ''
+    };
   }
 
   render() {
-    return <div />;
+    return (
+      <div className="user-input-wrap">
+        <input className="idea-title" type="text" />
+        <input className="idea-body" type="text" />
+        <button>save</button>
+      </div>
+    );
   }
 }
