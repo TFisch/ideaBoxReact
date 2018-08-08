bc062e532045e5fcc2e70cc1b33c9097e5c36f49
Tim Fischer <emailtimfischer@gmail.com>
2018-08-08 13:06:35 -0600

    Style save button

--- src/components/Input.js
+++ src/components/Input.js
@@ -14,9 +14,9 @@ export default class Input extends Component {
     return (
       <div className="user-input-wrap">
         <h2>Ideabox</h2>
-        <input className="idea-title entry" type="text" />
-        <input className="idea-body entry" type="text" />
-        <button>save</button>
+        <input className="idea-title entry input-box" type="text" />
+        <input className="idea-body entry input-box" type="text" />
+        <button className="save-button entry">save</button>
       </div>
     );
   }
