146354edfe4771bffb220225a32dc0775050c1ae
Tim Fischer <emailtimfischer@gmail.com>
2018-08-08 13:18:09 -0600

    Save idea title and body in state

--- src/components/Input.js
+++ src/components/Input.js
@@ -10,13 +10,36 @@ export default class Input extends Component {
     };
   }
 
+  handleTitleChange(e) {
+    this.setState({ titleInput: e.target.value });
+  }
+
+  handleBodyChange(e) {
+    this.setState({ bodyInput: e.target.value });
+  }
+
   render() {
     return (
       <div className="user-input-wrap">
         <h2>Ideabox</h2>
-        <input className="idea-title entry input-box" type="text" />
-        <input className="idea-body entry input-box" type="text" />
-        <button className="save-button entry">save</button>
+        <input
+          className="idea-title entry input-box"
+          type="text"
+          onChange={e => this.handleTitleChange(e)}
+        />
+        <input
+          className="idea-body entry input-box"
+          type="text"
+          onChange={e => this.handleBodyChange(e)}
+        />
+        <button
+          className="save-button entry"
+          onClick={() =>
+            this.props.addIdea(this.state.titleInput, this.state.bodyInput)
+          }
+        >
+          save
+        </button>
       </div>
     );
   }
