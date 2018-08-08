319a5e735bec6da6d5219575627ae1e461a3c7c0
Tim Fischer <emailtimfischer@gmail.com>
2018-08-08 15:40:13 -0600

    Create add idea function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import './App.css';
 import Input from './components/Input';
 
 class App extends Component {
+  
+  addIdea = idea => {
+    const newIdea = {...idea, id: Date.now()}
+    const ideas = [...this.state.ideas, newIdea]\
+    this.setState({ ideas })
+  }
+  
+  
+  
+  
   render() {
     return (
       <div className="App">
